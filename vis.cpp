1edfe48ea9e7e003cfe829e72c3d123f18759692
Gilberto Gabon <gagabon501@yahoo.com>
2022-06-03 07:22:30 +1200

    Created the system menu

---
+++ vis.cpp
@@ -0,0 +1,31 @@
+/***********************************************************************************
+ * Title        : CS-103 Integrated Studio I Assessment 2: Vehicle Insurance System
+ * File         : vis.cpp
+ * Purpose      : Entry program for the Vehicle Insurance System
+ * Parameters   : None
+ * Returns      : Status of program execution: 0=Success Non-zero=Error
+ ************************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Structure definitions here
+
+// Function prototypes here
+
+int main()
+{
+    cout << "\n=============================\n";
+    cout << "  Vehicle Insurance System\n";
+    cout << "=============================\n";
+    cout << "1. Manage Clients\n";
+    cout << "2. Manage Insurance Policies\n";
+    cout << "3. Manage Premiums\n";
+    cout << "4. Manage Claims\n";
+    cout << "5. System Reports\n";
+    cout << "6. System Utilities\n";
+    cout << "=============================\n";
+    return 0;
+}
