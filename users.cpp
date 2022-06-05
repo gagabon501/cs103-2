260a5b7083a3c9192e067f32a3f434056e184656
Gilberto Gabon <gagabon501@yahoo.com>
2022-06-05 23:59:26 +1200

    Done user registration and login. Passwords are saved encrypted

--- users.cpp
+++ users.cpp
@@ -9,99 +9,165 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <conio.h>
+#include <limits>
 #include "users.h"
+
 using namespace std;
 
 // Structure definitions here
 
-// Define a user structure here: firstname, lastname, phone, email, car make, car color, car rego
+// Define a user structure here: email, password, firstname, lastname, phone
 struct User
 {
-    string username;
+    string email;
     string password;
     string firstname;
     string lastname;
     string phone;
-    string email;
-    string carMake;
-    string carColor;
-    string carRego;
 
     // Constructor to initialize members
-    User(string uname = "", string passwd = "", string fname = "", string lname = "", string ph = "", string em = "", string cmake = "", string ccolor = "", string crego = "")
+    User(string em = "", string passwd = "", string fname = "", string lname = "", string ph = "")
     {
-        username = uname;
+
+        email = em;
         password = passwd;
         firstname = fname;
         lastname = lname;
         phone = ph;
-        email = em;
-        carMake = cmake;
-        carColor = ccolor;
-        carRego = crego;
     }
 };
 
-vector<User> inputUsers(vector<User> &user)
+// Function Prototypes here
+
+bool checkDuplicate(string email, vector<User> frmUsersFile);
+vector<User> readFile();
+void writeFile(struct User user);
+int doLogin();
+int doRegister();
+int showLoginMenu();
+string getPasswd(string &passwd, string textPrompt);
+string doEncrypt(string text);
+
+void registerUser(struct User user)
 {
-    struct User u;
-    char ans = 'y';
-    while (tolower(ans) == 'y')
+    vector<User> userFile;
+
+    userFile = readFile();
+
+    cout << "    Email: ";
+    cin >> user.email;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    if (!checkDuplicate(user.email, userFile))
     {
-        cout << " Username: ";
-        cin >> u.username;
-        cout << " Password: ";
-        cin >> u.password;
-        cout << " Lastname: ";
-        cin >> u.lastname;
+        getPasswd(user.password, " Password: "); // masks wit 'x' the inputted password
+
+        cout << " \nLastname: ";
+        cin >> user.lastname;
         cout << "Firstname: ";
-        cin >> u.firstname;
+        cin >> user.firstname;
         cout << "    Phone: ";
-        cin >> u.phone;
-        cout << "    Email: ";
-        cin >> u.email;
-        cout << " Car Make: ";
-        cin >> u.carMake;
-        cout << "Car Color: ";
-        cin >> u.carColor;
-        cout << " Car REGO: ";
-        cin >> u.carRego;
+        cin >> user.phone;
 
-        user.push_back(u); // save vector array (u)
-        cout << "Continue (y/n)? ";
-        cin >> ans;
+        // Add user to users.csv file
+        writeFile(user);
     }
-
-    return (user);
+    else
+    {
+        cout << "\nUsername (email) already existing in the database. Please use another email to sign-up\n";
+    };
 }
 
-void writeFile(vector<User> &u)
+string getPasswd(string &passwd, string textPrompt)
 {
-    fstream userFile("users.csv", ios::app);
-    for (int i = 0; i < (int)u.size(); i++)
+    int ch;
+
+    string textInput = "";
+
+    cout << textPrompt;
+
+    while (true)
     {
-        userFile << u[i].username << "," << u[i].password << "," << u[i].lastname << "," << u[i].firstname << "," << u[i].phone << "," << u[i].email << "," << u[i].carMake << "," << u[i].carColor << "," << u[i].carRego << "," << endl;
+        ch = getch();
+        if (ch == 10)
+        {
+            break;
+        }
+        else
+        {
+            textInput.push_back(ch);
+        }
+        cout << 'x';
+    }
+
+    // cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    passwd = doEncrypt(textInput);
+
+    return passwd; // returned string is encrypted already
+}
+
+string doEncrypt(string text)
+// Simple encryption of the text input (just adding 10 to the ascii code of the every character)
+{
+
+    for (int i = 0; i < (int)sizeof(text); i++)
+    {
+        text[i] += 10;
+    }
+    return text;
+}
+
+vector<User> readFile()
+{
+    vector<User> tmpUser;
+    struct User user;
+    string txtLine;
+
+    fstream userFile("users.csv", ios::in);
+
+    while (getline(userFile, txtLine))
+    {
+        istringstream linestream(txtLine); // to split the row into columns/properties
+        string item;
+        getline(linestream, item, ',');
+        user.email = item;
+        getline(linestream, item, ',');
+        user.password = item;
+        getline(linestream, item, ',');
+        user.lastname = item;
+        getline(linestream, item, ',');
+        user.firstname = item;
+        getline(linestream, item, ',');
+        user.phone = item;
+        tmpUser.push_back(user);
     }
     userFile.close();
+    return tmpUser;
 }
 
-void showUsers(vector<User> &user)
+bool checkDuplicate(string email, vector<User> frmUsersFile)
 {
-    for (int i = 0; i < (int)user.size(); i++)
+    bool retVal = false;
+
+    for (int i = 0; i < (int)frmUsersFile.size(); i++)
     {
-        cout << user[i].username << endl;
-        cout << user[i].password << endl;
-        cout << user[i].firstname << endl;
-        cout << user[i].lastname << endl;
-        cout << user[i].phone << endl;
-        cout << user[i].email << endl;
-        cout << user[i].carMake << endl;
-        cout << user[i].carColor << endl;
-        cout << user[i].carRego << endl;
+        if (frmUsersFile[i].email == email)
+        {
+            retVal = true;
+        }
     }
 
-    writeFile(user);
+    return retVal;
+}
+
+void writeFile(struct User user)
+{
+    fstream userFile("users.csv", ios::app); // open file in append mode
+    userFile << user.email << "," << user.password << "," << user.lastname << "," << user.firstname << "," << user.phone << endl;
+    userFile.close();
 }
 
 int doLogin()
@@ -109,17 +175,31 @@ int doLogin()
     string username = "";
     string password = "";
     int retVal = 0, tries = 0;
+    vector<User> userFile;
+
+    userFile = readFile();
+
     while (tries != 3)
     {
-        cout << "Enter username: ";
+        cout << "Enter username (email): ";
         cin >> username;
-        cout << "Enter password: ";
-        cin >> password;
-        // Open users.csv file then search for the username and password (utilize file operations)
-        // if username and password match then return 1 otherwise return 0
-        if (username == "gil" && password == "guapo")
+        // cout << "        Enter password: ";
+        // cin >> password;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        getPasswd(password, "        Enter password: "); // masks wit 'x' the inputted password
+
+        for (int i = 0; i < (int)userFile.size(); i++)
         {
-            retVal = 1;
+            if (userFile[i].email == username && userFile[i].password == password)
+            {
+                retVal = 1;
+                break;
+            }
+        }
+
+        if (retVal == 1)
+        {
+
             break;
         }
         else
@@ -137,14 +217,13 @@ int doLogin()
 
 int doRegister()
 {
-    vector<User> users;
+    struct User user;
 
     cout << "=======================================================\n";
     cout << "Welcome to Vehicle Insurance System - User Registration\n";
     cout << "=======================================================\n";
 
-    users = inputUsers(users);
-    showUsers(users);
+    registerUser(user);
 
     return 2;
 }
